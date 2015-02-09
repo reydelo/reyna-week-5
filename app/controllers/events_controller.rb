270401dc96712f7bf876205908d71e9aba8b4849
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-09 11:18:39 -0700

    users can see location show page and create locations

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,19 @@
 class EventsController<ApplicationController
 
   def new
+    @event = Event.new
+    @location = Location.find(params[:location_id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    @event.location_id = params[:event_id]
+    if @event.save
+      redirect_to locations_path
+    else
+      render @location
+    end
   end
 
   def show
@@ -8,5 +21,10 @@ class EventsController<ApplicationController
 
   def edit
   end
-  
+
+  private
+  def event_params
+    params.require(:event).permit(:description, :date, :requires_id)
+  end
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,15 +1,45 @@
 class LocationsController<ApplicationController
-  
+
   def index
+    @locations = Location.all
   end
 
   def new
+    @location = Location.new
+  end
+
+  def create
+    @location=Location.new(location_params)
+    if @location.save
+      redirect_to locations_path, notice: 'Location was successfully created.'
+    end
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to locations_path, notice: 'Location was successfully updated.'
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    if @location.destroy
+      redirect_to locations_path, notice: 'Location was successfully deleted.'
+    end
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name, :address, :zipcode)
   end
 
 end
